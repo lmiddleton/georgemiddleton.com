9df10f1cbc04eb21f6bccd9fcd6c3dbebea3afca
Lauren Middleton <git@laurenmiddleton.com>
2015-04-16 21:39:19 -0400

    factored out image page load functions

--- prints.php
+++ prints.php
@@ -6,36 +6,9 @@ require_once 'functions.php';
 require_once 'Savant3-3.0.1/Savant3-3.0.1/Savant3.php';
 $tpl = new Savant3();
 
-// specify the file containing the page guts to show
-$file = "templates/image-page-guts.tpl.php";
-
 // specify the image category
 $category = "prints";
 
-// get and store the paintings
-$paintings = get_paintings();
-
-// find the current image that should be displayed
-$image = get_current_image($paintings, $category);
-
-// find the next image in the category
-$next_image = get_next_image($paintings, $image, $category);
-
-// build the full image
-$full_image = build_full_img($paintings, $image, $next_image, $category);
-
-// build the thumb nav
-$thumbnav = build_thumbnav($paintings, $image, $category);
-
-// Assign values to the Savant instance.
-$tpl->file = $file;
-$tpl->category = $category;
-$tpl->image = $image;
-$tpl->next_image = $next_image;
-$tpl->full_image = $full_image;
-$tpl->thumbnav = $thumbnav;
-
-// Display a template using the assigned values.
-$tpl->display('templates/page.tpl.php');
+load_image_page($category, $tpl);
 
 ?>
