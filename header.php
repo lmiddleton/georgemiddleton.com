870c97af497d0c899424fbb3367352074042223e
Lauren Middleton <git@laurenmiddleton.com>
2015-05-31 14:31:17 -0400

    updated menu

--- header.php
+++ header.php
@@ -6,11 +6,15 @@
 	<img src="assets/Divider.gif" alt="" />
 	<a href="prints.php" class="text">Prints</a>
 	<img src="assets/Divider.gif" alt="" />
-	<a href="about.php" class="text">About/Exhibits</a>
+	<a href="illustration.php" class="text">Illustration</a>
 	<img src="assets/Divider.gif" alt="" />
-	<a href="contact.php" class="text">Contact/Newsletter</a>
+	<a href="about.php" class="text">About</a>
+	<img src="assets/Divider.gif" alt="" />
+	<a href="contact.php" class="text">Contact</a>
+	<!--
 	<img src="assets/Divider.gif" alt="" />
 	<a href="sales.php" class="text">Sales</a>
+	-->
 
 	<a href="https://www.facebook.com/pages/George-Middleton-Artist/320877231266484?ref=aymt_homepage_panel" target="_blank" class="img"><img src="assets/FacebookGray.gif" alt="Facebook" /></a>
 	<a href="https://plus.google.com/112853349605560241157/about" target="blank" class="img"><img src="assets/GooglePlusGray.gif" alt="Google Plus" /></a>
