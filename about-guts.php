8202d10de7b406b092a212b227ad4a7c0d3c6038
Lauren Middleton <git@laurenmiddleton.com>
2015-07-09 00:08:33 -0400

    updated about and contact pages

--- about-guts.php
+++ about-guts.php
@@ -1,18 +1,17 @@
 <div class="text-container">
 	<p>
-		I work from the long history and tradition of artists inspired by spiritual experiences, religious ideas, and a love of nature. Finding ways within representational painting to suggest or evoke this aspect of the human experience is the main exploration, the main challenge of my practice.
+		I grew up in an idyllic country setting, surrounded by woods, fields, and a river in the back yard. My youthful obsession was bird watching, and all my early paintings were bird portraits. I learned the basics of watercolor from my mother; we were an artistic family, always tramping about, collecting specimens to sketch. Drawing and painting became the natural expression for this joyful existence.
 	</p>
 
 	<p>
-		I think there has always been an active and deep yearning in people for connection- to go inward and seek transformation through prayer, meditation, devotion, or ritual movement. By imagining the figure- the person- as a fluid force of energy in ceremonial concert with the elements, I aim to suggest the divine impulse, this relentless pressure of the spirit.
+		With the exaltation of my childhood, my constant feeling was- there must be some hidden intelligent force behind this beautiful Life. I wanted to paint this feeling. Later on, I came under the spell of various alternative spiritual ideas and visionary artists, and together with an abiding love of nature, the main direction of my painting was influenced and set in this fashion.
 	</p>
 
 	<p>
-		Painting for me is like a celebration- an action of return to innocence and clarity. <br />
-		Indeed, I love the craft, the decorative expression, and the technical issues of painting- the sheer pleasure of making something of quality with my own hands. I strive to create the beautiful object, an icon imbued with these feelings..
+		As I see it, my work is about the invisible connection we have with that force. The basic emphasis is to treat the figure as fluid, receptive energy engaged in ritual concert with the elements. I imagine this as an experience where the sense of personal separation is relaxed and transformed by a ceremonial interaction with the living landscape. And thus, contact with the primal source becomes possible- and with it, a renewal of vitality, clarity, peace, and harmony.
 	</p>
 
 	<p id="bio-link">
-		<a href="">Bio and CV.pdf</a>
+		<a href="resume/GeorgeMiddleton.pdf">Resume.pdf</a>
 	</p>
 </div>
--- contact-guts.php
+++ contact-guts.php
@@ -1,16 +1,26 @@
 <div class="text-container">
 	<p style="text-align: center;">
 		<a href="mailto:info@georgemiddleton.com">info@georgemiddleton.com</a><br />
+	</p>
+	<p style="text-align: center;">
+		George Middleton Art Studio<br />
+		PO Box 626<br />
+		Carlisle, MA 01741<br />
 		(508) 254-5489
 	</p>
 	<p style="text-align: center;">
-		George Middleton<br />
-		PO Box 626<br />
-		Carlisle, MA 01741<br />
+		<a href="">Purchasing Information</a>
 	</p>
 	<div>
 		<p>
-			Subscribe to my studio newsletter for advanced notice of new work and exhibitions.
+			Please feel free to contact me with any questions, for painting purchases, and for a discount on orders of two or more prints. I am also available for commissioned work and illustration projects, and certain works are available for licensing or usage.
+		</p>
+		<p>
+			Hi-resolution photos are available for all paintings for sale. Paintings on gallery-wrapped stretcher frames or cradled panels have hand-finished edges, and are suitable for display "out of the box". All prints are made with archival materials, are hand-signed, numbered, and dated, and include a certificate of authenticity. Payment is through Paypal. Shipping cost is included for prints, extra for paintings. Mass. customers add sales tax. Please allow two weeks for delivery. If you are dissatisfied for any reason with your purchase, or the artwork is damaged, return the work for a full refund within 14 days from the day you receive it. You pay the return shipping cost.
+		</p>
+
+		<p>
+			Please subscribe to my studio newsletter for advanced notice of new work and exhibits.
 		</p>
 
 		<!-- Begin MailChimp Signup Form -->
@@ -44,22 +54,6 @@
 		<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
 		<script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
 		<!--End mc_embed_signup-->
-
-		<p>
-			<strong>Terms and Conditions:</strong> All artwork is made with the highest quality archival materials and methods. Paintings on gallery-wrapped stretcher frames or cradled panels have hand-finished edges, and are suitable for display "out of the box". Larger detail photos of the paintings are available upon request. Inkjet prints are made with archival pigment inks and papers, are hand-signed, numbered, and dated, and include a certificate of authenticity, and a half-inch border for framing. Prints, drawings, and paintings on paper should be framed for long-term care and protection. All framing is the responsibility of the purchaser. My work is available for licensing and usage, and I am available for commissioned work.
-		</p>
-
-		<p>
-			<strong>Purchase and payment:</strong> You can purchase individual prints from this website. Please contact me directly for all painting purchases, and for orders of two or more prints. Payment is by Paypal only. Massachusetts customers add 6.25% sales tax.
-		</p>
-
-		<p>
-			<strong>Shipping:</strong> For paintings the cost is extra and via FedEx, with a signature required upon delivery. For prints, shipping is included and via USPS. Local delivery (Eastern Massachusetts) of paintings by ground car is available and free. Otherwise, please allow two weeks to receive your order. All works are shipped in flat containers.
-		</p>
-
-		<p>
-			<strong>Returns and refunds:</strong> If you are dissatisfied for any reason with your purchase, or if the artwork is damaged by shipping, you may return the work for a full refund within 14 days from the day you receive the shipment. You pay the return shipping cost.
-		</p>
 		
 	</div>
 </div>
